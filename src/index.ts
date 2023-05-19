9e410b2a454fdba137f0fb7fa1ab65376c375b5a
Lasse Becker <lasse@cpsh.de>
2023-05-19 20:57:04 +0200

    refactor: pulld union into SelectSingleTypeOfUnion args

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,33 @@ type CommonResultType = string;
 type Union = TimespanDto | TimeDto | DateSpanDto | DateDto;
 type DiscriminatorKey = "object";
 type DiscriminatorValue = Union[DiscriminatorKey];
-type SelectSingleTypeOfUnion<K extends DiscriminatorValue> = Extract<
+type SelectSingleTypeOfUnion<Union, K extends DiscriminatorValue> = Extract<
   Union,
   { object: K }
 >;
 type FuncMap = {
   [K in DiscriminatorValue]: (
-    arg: SelectSingleTypeOfUnion<K>,
+    arg: SelectSingleTypeOfUnion<Union, K>,
   ) => CommonResultType;
 };
 
-function MatchFunction(translateWhenMap: FuncMap) {
+function MatchFunction(funcMap: FuncMap) {
   function result<K extends DiscriminatorValue>(
-    when: SelectSingleTypeOfUnion<K>,
+    when: SelectSingleTypeOfUnion<Union, K>,
   ) {
     const obj: K = when.object;
-    return translateWhenMap[obj](when);
+    return funcMap[obj](when);
   }
   return result;
 }
 
-const translateWhenMap: FuncMap = {
+const funcMap: FuncMap = {
   timespan: translateTimeSpan,
   date: translateDate,
   datespan: translateDateSpan,
   time: translateTime,
 };
-const translate = MatchFunction(translateWhenMap);
+const translate = MatchFunction(funcMap);
 const translateInlineArgs = MatchFunction({
   timespan: translateTimeSpan,
   date: translateDate,
@@ -79,25 +79,8 @@ function translateDate(when: DateDto): CommonResultType {
 
 translate({ object: "date", date: "abc" }); // okay
 
-const badTranslateWhenMap: {
-  [K in DiscriminatorValue]: (
-    arg: SelectSingleTypeOfUnion<K>,
-  ) => CommonResultType;
-} = {
+translate({
   // @ts-expect-error
-  timespan: translateDateSpan, // error
-  date: translateDate,
-  // @ts-expect-error
-  datespan: translateTimeSpan, // error
-  time: translateTime,
-};
-
-function badTranslate<K extends DiscriminatorValue>(
-  when: SelectSingleTypeOfUnion<K>,
-) {
-  const obj: K = when.object;
-  // @ts-expect-error
-  const badWhen: Date = { object: "date", date: "Next Thursday" };
-  // @ts-expect-error
-  return translateWhenMap[obj](badWhen); // error!
-}
+  object: "deate",
+  date: "abc",
+});
